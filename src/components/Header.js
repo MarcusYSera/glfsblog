0e49756e662f6754d46b43f70fdc07940941ed55
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-26 05:12:20 -0700

    did something

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/" className="item">
+    <div className="ui segment">
+      <Link to="/" className="ui left aligned header">
         App Icon
       </Link>
-      <Link to="/" className="item">
-        <i className="home icon" />
+      <Link to="/" className="ui right aligned header">
+        Home
+        {/* <i className="home icon" /> */}
       </Link>
+      <Link to="/">Login</Link>
+      <Link to="/">Sign Up</Link>
     </div>
   );
 };
