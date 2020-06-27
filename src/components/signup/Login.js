9bb832cfdee6497a35ce74487e4dc1fb23059658
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-26 22:10:31 -0700

    working out the form inputs

--- src/components/signup/Login.js
+++ src/components/signup/Login.js
@@ -1,18 +1,28 @@
 import React from 'react';
+// import InputField from './InputField';
 
-const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <input type="text" />
-        <br />
-        <input type="text" />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { answer: '' };
+  }
+
+  inputValue(event) {
+    this.setState({ answer: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={(event) => console.log(event.target.value)}>
+          Email:
+          <input type="text" value={this.state.answer} onChange={this.inputValue} />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
