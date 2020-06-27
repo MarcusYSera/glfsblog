14084e64afe47f06747f859d3d606fd47c6a3796
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-26 23:35:54 -0700

    working on destructuring

--- src/components/signup/Login.js
+++ src/components/signup/Login.js
@@ -2,22 +2,20 @@ import React from 'react';
 // import InputField from './InputField';
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { answer: '' };
-  }
+  state = {};
 
   inputValue(event) {
     this.setState({ answer: event.target.value });
   }
 
   render() {
+    const { answer } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={(event) => console.log(event.target.value)}>
           Email:
-          <input type="text" value={this.state.answer} onChange={this.inputValue} />
+          <input type="text" value={answer} onChange={this.inputValue} />
           <input type="submit" value="Submit" />
         </form>
       </div>
