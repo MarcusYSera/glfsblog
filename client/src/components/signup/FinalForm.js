da39e95651bc18b494c91b11e888eae6020f0fca
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-09 19:09:39 -0700

    final form practice

--- client/src/components/signup/FinalForm.js
+++ client/src/components/signup/FinalForm.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { Form, Field, FormSpy } from 'react-final-form';
 
 class FinalForm extends Component {
-
-  onSubmit=(e)=>{
+  onSubmit = (e) => {
     e.preventDefault();
     console.log(e);
-  }
+  };
 
   render() {
     return (
@@ -14,7 +13,15 @@ class FinalForm extends Component {
         <h1>Final Form</h1>
         <Form>
           onSubmit={this.onSubmit}
-
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>First Name</label>
+              <Field name="firstName" type="text" placeholder="First Name" />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </Form>
       </div>
     );
