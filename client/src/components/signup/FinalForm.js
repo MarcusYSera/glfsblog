1b45101ef9ea1337a41f080d093fa2378be0c62f
Marcus Sera <m.yoshiosera@gmail.com>
2020-07-09 19:05:48 -0700

    testing out form tags

--- client/src/components/signup/FinalForm.js
+++ client/src/components/signup/FinalForm.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { Form, Field, FormSpy } from 'react-final-form';
 
 class FinalForm extends Component {
+
+  onSubmit=(e)=>{
+    e.preventDefault();
+    console.log(e);
+  }
+
   render() {
     return (
       <div>
         <h1>Final Form</h1>
+        <Form>
+          onSubmit={this.onSubmit}
+
+        </Form>
       </div>
     );
   }
