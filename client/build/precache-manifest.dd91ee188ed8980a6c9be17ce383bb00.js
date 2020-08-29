d7cfdf50ca7fc05541fa8e3cf2da82aff5528cef
Marcus Sera <m.yoshiosera@gmail.com>
2020-08-28 17:35:59 -0700

    setting up express path

---
+++ client/build/precache-manifest.dd91ee188ed8980a6c9be17ce383bb00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2a0d1fc273d8598a42f0027b398698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e4e5bfd664bd18d0eba",
+    "url": "/static/css/main.05006018.chunk.css"
+  },
+  {
+    "revision": "a2e04451f5e347c69a42",
+    "url": "/static/js/2.1b4e7629.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b4e7629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4e5bfd664bd18d0eba",
+    "url": "/static/js/main.bf86005a.chunk.js"
+  },
+  {
+    "revision": "71b838318ba7c8641052",
+    "url": "/static/js/runtime-main.cf417be9.js"
+  },
+  {
+    "revision": "6233af99c4dd578ec0a0b0f4d3009ced",
+    "url": "/static/media/saku.6233af99.svg"
+  }
+]);
